c91e1bca18658b3da9e56f6089429fb558c37c6d
mimvoid <mimvoid@proton.me>
2025-11-30 18:55:29 -0500

    Update to v0.3.0

--- spritesExporter/sprites_exporter.py
+++ spritesExporter/sprites_exporter.py
@@ -8,7 +8,7 @@ from builtins import Scripter
 from .controller import Controller
 
 
-SPRITESHEET_EXPORTER_VERSION = "0.2.0"
+SPRITESHEET_EXPORTER_VERSION = "0.3.0"
 
 
 class SpritesExporter(Extension):
